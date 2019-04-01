05a35528765ff217aadf8d2b705e877e5f45619d
Akin Oladeji <lahdayg@yahoo.com>
2019-04-02 00:36:00 +0100

    Notes Updated

---
+++ AutomationTesterInterviewExercise/Into.Digital.AutomationTesterInterviewExercise/DriverArrange.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Into.Digital.AutomationTesterInterviewExercise
+{
+    /// <summary>
+    ///     Initializes the Selenium driver, can be inhertied from <see cref="DriverArrange"/>.
+    /// </summary>
+    public class DriverArrange
+    {
+        /// <summary>
+        ///     Provides an instance of the <see cref="IWebDriver"/>.
+        /// </summary>
+        public IWebDriver driver;
+
+        /// <summary>
+        ///     Constructor, into which we inject any dependencies.
+        /// </summary>
+        public DriverArrange()
+        {
+            var startupAssembly = typeof(DriverArrange).GetTypeInfo().Assembly;
+            var root = GetProjectPath("", startupAssembly);
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments(new List<string>() { "start-maximized" });
+            driver = new ChromeDriver(root, chromeOptions);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+    
+        /// <summary>
+        ///     Retrieves project path and directory info for driver setup.
+        /// </summary>
+        /// <param name="projectRelativePath"></param>
+        /// <param name="startupAssembly"></param>
+        /// <returns></returns>
+        private static string GetProjectPath(string projectRelativePath, Assembly startupAssembly)
+        {
+            var projectName = startupAssembly.GetName().Name;
+            var applicationBasePath = AppContext.BaseDirectory;
+            var directoryInfo = new DirectoryInfo(applicationBasePath);
+            do
+            {
+                directoryInfo = directoryInfo.Parent;
+
+                var projectDirectoryInfo = new DirectoryInfo(Path.Combine(directoryInfo.FullName, projectRelativePath));
+                if (projectDirectoryInfo.Exists)
+                {
+                    var projectFileInfo = new FileInfo(Path.Combine(projectDirectoryInfo.FullName, projectName, $"{projectName}.csproj"));
+                    if (projectFileInfo.Exists)
+                    {
+                        return Path.Combine(projectDirectoryInfo.FullName, projectName);
+                    }
+                }
+            }
+            while (directoryInfo.Parent != null);
+
+            throw new Exception($"Project root could not be located using the application root {applicationBasePath}.");
+        }
+
+        /// <summary>
+        ///     Helper function for async operations.
+        /// </summary>
+        /// <param name="waitTime">The duration of time, in milliseconds, that the thread should sleep for.</param>
+        public void Wait(int waitTime)
+        {
+            System.Threading.Thread.Sleep(waitTime);
+        }
+
+        /// <summary>
+        ///     Removed, not being used.
+        /// </summary>
+        // public void ScrollDown(int keyDown)
+        // { 
+               // Page load and scroll down
+        //     for (int i = 0; i < keyDown; i++)
+        //     {
+        //         driver.SwitchTo().ActiveElement().SendKeys(Keys.Down);
+        //        Wait(50);
+        //    }
+
+        //    Wait(4000);
+        // }
+
+        public bool OpenBrowser()
+        {
+            try
+            {
+                driver.Navigate().GoToUrl("https://www.intostudy.com/en-gb");
+                
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            } 
+        }
+    }
+}
---
+++ AutomationTesterInterviewExercise/Into.Digital.AutomationTesterInterviewExercise/IntoStudyAutomationTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using OpenQA.Selenium;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Into.Digital.AutomationTesterInterviewExercise
+{
+    /// <summary>
+    ///     This is the entry point for automation tests for the INTO Study platform.
+    /// </summary>
+    public class IntoStudyAutomationTests : DriverArrange
+    {
+        /// <summary>
+        ///     Injected to provide an instance of the <see cref="ITestOutputHelper"/>
+        /// </summary>
+        /// <remarks>The <see cref="ITestOutputHelper"/> is preferred over <see cref="Console"/> writes.</remarks>
+        private readonly ITestOutputHelper _theOutputHelper;
+
+        /// <summary>
+        ///     Constructor, into which we inject any dependencies.
+        /// </summary>
+        /// <param name="outputHelper"></param>
+        public IntoStudyAutomationTests(ITestOutputHelper theOutputHelper)
+        {
+            _theOutputHelper = theOutputHelper;
+        }
+
+        /// <summary>
+        ///     This tests the University pages in the following steps:
+        ///         * Loads INTO Study homepage.
+        ///         * Navigates to the Universities page.
+        ///         * Clicks "View More" CTA on the university card. 
+        /// </summary>
+        [Fact]
+        public void ClickingTheViewMoreButtonLoadsAUniversityPage()
+        {
+            // Opens browser and navigates to the INTO Study homepage.
+            if (OpenBrowser())
+            {
+                _theOutputHelper.WriteLine("Opening browser.");
+
+                // Wait.
+                Wait(2000);
+
+                // Navigates to Universities page.
+                driver.FindElement(By.LinkText("Universities")).Click();
+
+                _theOutputHelper.WriteLine("Navigating to the Universities page.");
+
+                // Wait.
+                Wait(2000);
+
+                // Selects "View More" on the university card.
+                driver.FindElements(By.CssSelector(".l-cta.cta"))[0].Click();
+
+                _theOutputHelper.WriteLine("Clicking 'View More' on the University card.");
+
+                // Wait.
+                Wait(2000);
+
+                // Closes the browser.
+                driver.Quit();
+
+                _theOutputHelper.WriteLine("Test Complete.");
+            }
+        }
+
+        /// <summary>
+        ///     This tests the changes the langauge from English to Chinese in the following steps:
+        ///         * Loads INTO Study homepage.
+        ///         * Change language to Chinese and zh-cn URL is displayed.
+        /// </summary>
+        [Fact]
+        public void ChangingLanguageWillUpdateQueryParameter()
+        {
+            if(OpenBrowser())
+            {
+                // Test language switch.
+            }
+        }
+    }
+}
